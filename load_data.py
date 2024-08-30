6a65776c614c499f2fd5da0a5c9f0284e7231354
Rutuja <rutujahande008@gmail.com>
2024-08-30 11:41:12 +0530

    Modular the Hotel Energy consumption Prediction

--- load_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-# Step 3: Load the dataset for SARIMA analysis
-file_path='daily_avg_kw_total.csv'
-def load_data(file_path):
-    df = pd.read_csv(file_path, parse_dates=['Date'], dayfirst=True)
-    df.set_index('Date', inplace=True)
-    return df
