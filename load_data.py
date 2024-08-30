daeecf30e9c873b383844e7253c1f82e402acf5f
AkashKawade45 <akashkawade0992@gmail.com>
2024-08-30 13:05:03 +0530

    Separate the Files

--- load_data.py
+++ load_data.py
@@ -4,4 +4,5 @@ file_path='daily_avg_kw_total.csv'
 def load_data(file_path):
     df = pd.read_csv(file_path, parse_dates=['Date'], dayfirst=True)
     df.set_index('Date', inplace=True)
+    df = df[:-5]
     return df
